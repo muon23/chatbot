33bfc6b38d5f4c53178d0c077edf4d2604961cfd
gongshaojie <gongshaojie@shanda.com>
2022-12-08 15:10:55 +0800

    1,Add apollo to save openai_key for security. 2,Update Dockerfile. 3,Add todo_reminder.txt

--- deployment/chatbot.py
+++ deployment/chatbot.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from pyapollo.apollo_client import ApolloClient
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src/main')))
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../commons/src/main')))
@@ -16,5 +17,18 @@ if len(sys.argv) < 2:
     exit(1)
 
 load_dotenv(f"{projectRoot}/deployment/{sys.argv[1]}/environment")
+
+app_id = os.environ.get("APP_ID")
+config_server_url = os.environ.get("CONFIG_SERVER_URL")
+authorization = os.environ.get("AUTHORIZATION")
+cache_file_path = os.environ.get("CACHE_FILE_PATH")
+env = os.environ.get("ENV")
+namespace = os.environ.get("NAMESPACE")
+openai_key = os.environ.get("OPENAI_KEY")
+
+client = ApolloClient(app_id=app_id, config_server_url=config_server_url, authorization=authorization, cache_file_path=cache_file_path, env=env)
+client.start()
+
+os.environ["OPENAI_KEY"] = client.get_value(openai_key, "default", namespace)
 
 ChatBotServer.main(debugArgs=["-c", f"{projectRoot}/deployment/{sys.argv[1]}/chatbot.yml"])
